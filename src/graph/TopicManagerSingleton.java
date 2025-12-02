0a7582983685453c24ac47ca5679d939e0705a78
Noam Malka <noamalka2903@gmail.com>
2025-12-02 19:17:41 +0200

    TopicManager

--- src/graph/TopicManagerSingleton.java
+++ src/graph/TopicManagerSingleton.java
@@ -1,10 +1,35 @@
-package test;
+package graph;
 
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class TopicManagerSingleton {
 
-    public static class TopicManager{
+    private static final TopicManager instance = new TopicManager();
+
+    public static TopicManager get() {
+        return instance;
     }
 
-    
+    public static class TopicManager {
+        private final Map<String, Topic> topics = new ConcurrentHashMap<>();
+
+        private TopicManager() { }
+
+        public Topic getTopic(String name) {
+            if (name == null) {
+                throw new IllegalArgumentException("Topic name cannot be null!");
+            }
+            return topics.computeIfAbsent(name, Topic::new);
+        }
+
+        public Collection<Topic> getTopics() {
+            return topics.values();
+        }
+
+        public void clear() {
+            topics.clear();
+        }
+    }
 }
