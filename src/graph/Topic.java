9a5f52f069567e415ee804ad7004755c227a1af1
Noam Malka <noamalka2903@gmail.com>
2025-12-02 19:17:19 +0200

    Added topic

--- src/graph/Topic.java
+++ src/graph/Topic.java
@@ -1,27 +1,47 @@
-package project_biu.graph;
+package graph;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Topic {
     public final String name;
-    Topic(String name){
-        this.name=name;
+    private final List<Agent> subscribers = new CopyOnWriteArrayList<>();
+    private final Set<Agent> publishers = ConcurrentHashMap.newKeySet();
+
+    Topic(String name) {
+        this.name = name;
     }
 
-    public void subscribe(Agent a){
-    }
-    public void unsubscribe(Agent a){
-    }
-
-    public void publish(Message m){
+    public void subscribe(Agent a) {
+        if (!subscribers.contains(a)) {
+            subscribers.add(verifyAgentNotNull(a));
+        }
     }
 
-    public void addPublisher(Agent a){
+    public void unsubscribe(Agent a) {
+        subscribers.remove(verifyAgentNotNull(a));
     }
 
-    public void removePublisher(Agent a){
+    public void publish(Message m) {
+        for (Agent a : subscribers) {
+            a.callback(name, m);
+        }
     }
 
+    public void addPublisher(Agent a) {
+        if (!publishers.contains(a)) {
+            publishers.add(verifyAgentNotNull(a));
+        }
+    }
 
+    public void removePublisher(Agent a) {
+        publishers.remove(verifyAgentNotNull(a));
+    }
+
+    private static Agent verifyAgentNotNull(Agent a) {
+        return Objects.requireNonNull(a, "Agent cannot be null");
+    }
 }
