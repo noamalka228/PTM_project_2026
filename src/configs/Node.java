727a72cca2ef47b97d885923587c74f42d37bbce
Noam Malka <noamalka2903@gmail.com>
2025-12-09 19:34:58 +0200

    Added getters and setters for Node

--- src/configs/Node.java
+++ src/configs/Node.java
@@ -2,6 +2,7 @@ package configs;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import graph.Message;
 
@@ -11,6 +12,40 @@ public class Node {
     private List<Node> edges;
     private Message msg;
 
+    public Node(String name) {
+        this.name = Objects.requireNonNull(name, "name cannot be null");
+        this.edges = new ArrayList<Node>();
+        this.msg = new Message("");
+    }
 
+    public String getName() {
+        return this.name;
+    }
+    public void setName(String newName) {
+        this.name = Objects.requireNonNull(newName, "name cannot be null");
+    }
 
+    public List<Node> getEdges() {
+        return this.edges;
+    }
+    public void setEdges(List<Node> newEdges) {
+        Objects.requireNonNull(newEdges, "edges cannot be null");
+        this.edges = new ArrayList<Node>(newEdges);
+    }
+
+    public Message getMessage() {
+        return this.msg;
+    }
+    public void setMessage(Message msg) {
+        this.msg = Objects.requireNonNull(msg, "message cannot be null");
+    }
+
+    public void addEdge(Node n) {
+
+    }
+
+    public boolean hasCycles(Node n) {
+        return true;
+    }
+    
 }
