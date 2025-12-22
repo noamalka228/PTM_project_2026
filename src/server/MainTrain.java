0524effffa4bc64327b0c08591197b996c83e9d4
Noam Malka <noamalka2903@gmail.com>
2025-12-22 21:22:31 +0200

    Removed unecesaary imports

--- src/server/MainTrain.java
+++ src/server/MainTrain.java
@@ -4,16 +4,11 @@ import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Random;
-
-import server.RequestParser.RequestInfo;
-import server.*;
 
 public class MainTrain { // RequestParser
 
